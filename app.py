f792055b2bc6319b741c3caf974384a90994e610
sayali-kadam <62012906+sayali-kadam@users.noreply.github.com>
2021-07-29 23:25:51 +0530

    API file

--- app.py
+++ app.py
@@ -47,32 +47,52 @@ def transcript_text(video_id):
     # translation = translation.text
 
 def summarized_text(transcript, video_id):
-    model = T5ForConditionalGeneration.from_pretrained("t5-base")
-    tokenizer = T5Tokenizer.from_pretrained("t5-base")
-    num_ite = int(len(transcript)/2000)
-    summarized_text = ""
-    for i in range(0, num_ite+1):
-        st = i * 2000
-        end = (i+1) * 2000
-        inputs = tokenizer.encode("summarize: " + transcript[st:end], return_tensors="pt", max_length=2000, truncation=True)
-        outputs = model.generate(inputs, max_length=500, min_length=100, length_penalty=2.0, num_beams=4, early_stopping=True)
-        temp = tokenizer.decode(outputs[0])
-        summarized_text += ' ' + temp
+    # model = T5ForConditionalGeneration.from_pretrained("t5-base")
+    # tokenizer = T5Tokenizer.from_pretrained("t5-base")
+    # num_ite = int(len(transcript)/2000)
+    # summarized_text = ""
+    # for i in range(0, num_ite+1):
+    #     st = i * 2000
+    #     end = (i+1) * 2000
+    #     inputs = tokenizer.encode("summarize: " + transcript[st:end], return_tensors="pt", max_length=2000, truncation=True)
+    #     outputs = model.generate(inputs, max_length=500, min_length=100, length_penalty=2.0, num_beams=4, early_stopping=True)
+    #     temp = tokenizer.decode(outputs[0])
+    #     summarized_text += ' ' + temp
 
-    return summarized_text
+    # return summarized_text
+    from sumy.parsers.html import HtmlParser
+    from sumy.parsers.plaintext import PlaintextParser
+    from sumy.nlp.tokenizers import Tokenizer
+    from sumy.summarizers.lsa import LsaSummarizer as Summarizer
+    from sumy.nlp.stemmers import Stemmer
+    from sumy.utils import get_stop_words
+
+    LANGUAGE = "english"
+    SENTENCES_COUNT = 3
+    import nltk;  
+    nltk.download('punkt')
+
+    parser = PlaintextParser.from_string(transcript, Tokenizer(LANGUAGE))
+    stemmer = Stemmer(LANGUAGE)
+
+    summarizer = Summarizer(stemmer)
+    summarizer.stop_words = get_stop_words(LANGUAGE)
+    s = ""
+    for sentence in summarizer(parser.document, SENTENCES_COUNT):
+      s += (str)(sentence)
+    return s
 
 
 @app.route('/api/summarize/<string:youtube_video>', methods=['GET','POST'])
 def YouTube_Video(youtube_video):
     transcript = transcript_text(youtube_video)
-    # summary = summarized_text(transcript, youtube_video)
+    summary = summarized_text(transcript, youtube_video)
     res = {
         "Video ID": youtube_video,
         "Transcript": transcript,
-    #     "Summary": summary
+        "Summary": summary
     }
     return jsonify(res)
-    # return "<p>Hello, World!</p>"
 
 if __name__ == "__main__":
     app.run(debug=True)
