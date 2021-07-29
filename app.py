1ebb41dae5cf3586751ab3d7287aab7dcc857da4
sayali-kadam <62012906+sayali-kadam@users.noreply.github.com>
2021-07-29 18:49:16 +0530

    API file

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ def summarized_text(transcript, video_id):
 @app.route('/api/summarize/<string:youtube_video>', methods=['GET','POST'])
 def YouTube_Video(youtube_video):
     transcript = transcript_text(youtube_video)
-    # summary = summarized_text(transcript, youtube_video)
+    summary = summarized_text(transcript, youtube_video)
     # res = {
     #     "Video ID": youtube_video,
     #     "Transcript": transcript,
